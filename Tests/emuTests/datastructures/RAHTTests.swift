b13b815a4bdbedd60d983639061e59134326241d
Tierry Hörmann <tierryhoermann96@gmx.ch>
2018-02-25 21:21:55 +0100

    Added a code skeleton to RAHTTests

--- Tests/emuTests/datastructures/RAHTTests.swift
+++ Tests/emuTests/datastructures/RAHTTests.swift
@@ -8,6 +8,53 @@
 import XCTest
 @testable import emu
 
+/**
+ This class tries to test all the functionality of RAHT by only assuming, that `insert` works.
+ */
 class RAHTTests: XCTestCase {
     
+    static var allTests = [
+        ("testCount", testCount),
+        ("testHas", testHas),
+        ("testRemove", testRemove),
+        ("testGet", testGet),
+        ("testGetRandom", testGetRandom),
+        ("testCustomGen", testCustomGen),
+        ("testCollection", testCollection),
+        ("testPerformance", testPerformance)
+    ]
+    
+    var r: RAHT
+    
+    func testCount() {
+        
+    }
+    
+    func testHas() {
+        
+    }
+    
+    func testRemove() {
+        
+    }
+    
+    func testGet() {
+        
+    }
+    
+    func testGetRandom() {
+        
+    }
+    
+    func testCustomGen() {
+        
+    }
+    
+    func testCollection() {
+        
+    }
+    
+    func testPerformance() {
+        
+    }
 }
